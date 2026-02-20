bf78a42a033412ee93bead444f2267536dd5589a
Roy Anger <roy@royanger.com>
2026-02-20 15:50:28 -0500

    feat: Added migrate support for better auth

--- src/transformers/index.ts
+++ src/transformers/index.ts
@@ -1,6 +1,7 @@
 import clerkTransformer from './clerk';
 import auth0Transformer from './auth0';
 import authjsTransformer from './authjs';
+import betterAuthTransformer from './betterauth';
 import firebaseTransformer from './firebase';
 import supabaseTransformer from './supabase';
 
@@ -8,6 +9,7 @@ export const transformers = [
 	clerkTransformer,
 	auth0Transformer,
 	authjsTransformer,
+	betterAuthTransformer,
 	firebaseTransformer,
 	supabaseTransformer,
 ];
